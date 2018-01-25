c8c267a8d6574f33df69eda0e70d38e03722d58a
Mintdot <nigthmare100@naver.com>
2018-01-25 10:54:04 +0900

    최저, 최고 기온 삭제

--- ssbot/bothub/weather.py
+++ ssbot/bothub/weather.py
@@ -1,25 +1,22 @@
+# -*- coding: utf-8 -*-
+
 import requests
 
 base_url = 'http://api.openweathermap.org/data/2.5/weather'
 
 
-def get_coord(lat, lon, app_id): # 좌표 받아오기
+def get_coord(lat, lon, app_id):  # 좌표 받아오기
     url = '{}?lat={}&lon={}&APPID={}'.format(base_url, lat, lon, app_id)
     response = requests.get(url)  # 데이터 긁어오기
     data = response.json()  # 딕셔너리 타입으로 변환
 
     name = data.get('name')
-    kelvin = 273.15
-    temp = data.get('main')
-    temp_average = temp['temp'] - kelvin
-    temp_min = temp['temp_min'] - kelvin
-    temp_max = temp['temp_max'] - kelvin
-    humidity = temp['humidity']
+    main = data.get('main')
+    temp = main['temp'] - 273.15  # 절대 온도로 불러오기 때문에 273.15만큼 빼줘야 섭씨 온도를 구할 수 있다.
+    humidity = main['humidity']
 
     message = "[{}의 날씨]\n" \
-              "평균 기온: {}℃\n" \
-              "최저 기온: {}℃\n" \
-              "최고 기온: {}℃\n" \
-              "습도: {}%".format(name, temp_average, temp_min, temp_max, humidity)
+              "기온: {}℃\n" \
+              "습도: {}%".format(name, temp, humidity)
 
     return message
